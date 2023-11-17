500c3c5ca00c3bbca513a80d5238e32c24185c0c
JSampereVázquez <72076376+jsamperevazquez@users.noreply.github.com>
2023-11-17 19:06:23 +0100

    Tarea 1_5 en progreso

--- Tarea1_5_DataFrames/data_frame.py
+++ Tarea1_5_DataFrames/data_frame.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 pd.to_datetime("20/01/2023", format="%d/%m/%Y")
 df = pd.read_csv(
@@ -19,3 +20,23 @@ row = len(df['Edad']) - 1
 for i in range(5):
     print(f" Edad fila {row + 1} : {df['Edad'].loc[row]}")
     row -= 1
+
+df = pd.read_csv(
+    'lista_ventas.csv',
+    encoding='windows-1252',
+    decimal=",",
+    parse_dates=['FechaNacimiento', 'FechaCompra'
+                 ],
+    index_col=['IDCliente']
+)
+
+print(df.tail())
+
+id_max = df['Importe'].idxmax()
+imp_max = df['Importe'].max()
+print(f"Codigo cliente: {id_max} con un importe de {imp_max}")
+
+df['Importe'] = pd.to_numeric(df['Importe'])
+df[df['Importe'] < 10].replace(10, inplace=True)
+
+
