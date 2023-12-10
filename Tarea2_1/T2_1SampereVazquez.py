e6089225088f38cbb61933ae9806db1651875617
JSampereVázquez <72076376+jsamperevazquez@users.noreply.github.com>
2023-12-10 18:11:58 +0100

    tarea 1 con csv terminada

---
+++ Tarea2_1/T2_1SampereVazquez.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import csv
+
+with open('fao.csv') as fao_file:
+    reader = csv.reader(fao_file)
+    headers = next(reader)
+
+header_indices = [i for i, item in enumerate(headers) if item]
+
+df_fao = pd.read_csv('fao.csv', usecols=header_indices, encoding='latin-1')
+df_fao['Date'] = pd.to_datetime(df_fao['Date'])
+print(df_fao)
+print(df_fao.dtypes)
+"""
+Debe calcular la media anual del precio de cada producto 
+en un nuevo dataframe añadiendo un ID adicional 
+de la forma ID_YYYY, siendo YYYY el año para cada producto.
+"""
+
+# Calculo la media agrupando por la fecha
+product_mean = df_fao.groupby(df_fao['Date'].dt.year).mean().round(decimals=2)
+
+print(product_mean)
+
+product_mean.set_index('Date', inplace=True)
+product_mean.rename(index=lambda x: x.strftime('ID_%Y'), inplace=True)
+
+print(product_mean)
+
