7e4265f7e765661bc6360c189698a6c240932244
JSampereVázquez <72076376+jsamperevazquez@users.noreply.github.com>
2023-12-11 18:04:08 +0100

    tarea 1 con xls terminada

--- Tarea2_1/T2_1SampereVazquez.py
+++ Tarea2_1/T2_1SampereVazquez.py
@@ -26,5 +26,22 @@ product_mean.rename(index=lambda x: x.strftime('ID_%Y'), inplace=True)
 
 print(product_mean)
 
-df_faoxls = pd.read_excel('fao.xls', keep_default_na=None, index_col=header_indices)
-print(df_faoxls)
+# Con Excel
+# Indico el nombre de la hoja que usaremos
+sheet = 'Indices_Monthly_Real'
+# Abro io con read_excel y hoja
+df_faoxls = pd.read_excel('fao.xls',
+                          keep_default_na=False, sheet_name=sheet)
+# Convierto object a datetime
+df_faoxls['Month'] = pd.to_datetime(df_faoxls['Month'])
+# Cambio nombre de cabecera
+df_faoxls.rename(columns={'Month': 'ID_YYYY'}, inplace=True)
+# Calculo la media agrupando por la fecha
+product_meanxls = df_fao.groupby(df_faoxls['ID_YYYY'].dt.year).mean().round(decimals=2)
+# Establezco índice
+product_meanxls.set_index('ID_YYYY', inplace=True)
+# Creo lambda para cambiar nombre de índice del DF
+product_meanxls.rename(index=lambda x: x.strftime('ID_%Y'), inplace=True)
+
+print("EXCEL")
+print(product_meanxls)
